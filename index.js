9438854d38995f0603ab6f011c00a3d8c85f6736
aniruth <aniruthvkumar@gmail.com>
2017-09-18 19:17:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-// Copyright 2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the 'License');
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS IS' BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+<?php
+$method=$_SERVER['REQUEST_METHOD'];
 
-'use strict';
-
-process.env.DEBUG = 'actions-on-google:*';
-const App = require('actions-on-google').ApiAiApp;
-const functions = require('firebase-functions');
-
-// [START YourAction]
-exports.yourAction = functions.https.onRequest((request, response) => {
-  const app = new App({request, response});
-  console.log('Request headers: ' + JSON.stringify(request.headers));
-  console.log('Request body: ' + JSON.stringify(request.body));
-
-  // Fulfill action business logic
-  function responseHandler (app) {
-    // Complete your fulfillment logic and send a response
-    app.tell('Hello, World!');
+//Process only when method is post
+if($method=="POST")
+{
+  $requestBody=file_get_contents('php://input');
+  $json=json_decode($requestBody);
+  $text=$json->result->parameters->text;
+  switch($text)
+  {
+    case 'hi':
+      $speech="Hi,Nice to meet you";
+      break;
+      case 'bye':
+        $speech="https://www.google.co.in/";
+        header('Location: '.$speech);
+        break;
+        case 'anything':
+          $speech="Yes,you can type anything here";
+          break;
+          default:
+            $speech="Sorry,I didnt get that .Please ask me something else";
+            break;
   }
+  $response=new \stdClass();
+  $response->speech=$speech;
+  $response->displayText=$speech;
+    $response->source="webhook";
+    echo(json_encode($response));
+    
+}
+else{
+  echo("Method not Allowed");
+}
 
-  const actionMap = new Map();
-  actionMap.set('input.welcome', responseHandler);
 
-  app.handleRequest(actionMap);
-});
-// [END YourAction]
+
+
+
+
+?>
