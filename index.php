e65f5622de87856e2bc6dca42bef179ac11b0e6c
aniruth <aniruthvkumar@gmail.com>
2017-09-20 10:52:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,15 +27,23 @@ if($method=="POST")
   $speech='<speak>
       <audio src=https://vocaroo.com/i/s0sROf0pL7RI></audio></speak>';
   
-  $response=new \stdClass();  
-  $response->speech=$speech;
-  //$response->source="webhook";
-  $response->data='google:{
+  $response={
+    speech: $speech,
+    data:{
+      google:{
         "expect_user_response": true,
         "is_ssml": true
-      }';
+      }
+    }
+  };  
+//   $response->speech=$speech;
+//   //$response->source="webhook";
+//   $response->data='google:{
+//         "expect_user_response": true,
+//         "is_ssml": true
+//       }';
   
-    echo(json_encode($response));
+    echo($response);
     
 }
 else{
