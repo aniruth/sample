b9c7dff737445e02d4ca868e6c121494a07ac29b
aniruth <aniruthvkumar@gmail.com>
2017-09-20 10:39:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,26 +7,29 @@ if($method=="POST")
   $requestBody=file_get_contents('php://input');
   $json=json_decode($requestBody);
   $text=$json->result->parameters->text;
-  switch($text)
-  {
-    case 'hi':
-      $speech="Hi,Nice to meet you";
-      break;
-      case 'bye':
-        $speech="<speak>
+//   switch($text)
+//   {
+//     case 'hi':
+//       $speech="Hi,Nice to meet you";
+//       break;
+//       case 'bye':
+//         $speech="<speak>
+//       <audio src=https://vocaroo.com/i/s0sROf0pL7RI></audio></speak>";
+//         break;
+//         case 'anything':
+//           $speech="Yes,you can type anything here";
+//           break;
+//           default:
+//             $speech="Sorry,I didnt get that .Please ask me something else";
+//             break;
+//   }
+  
+  $speech="<speak>
       <audio src=https://vocaroo.com/i/s0sROf0pL7RI></audio></speak>";
-        break;
-        case 'anything':
-          $speech="Yes,you can type anything here";
-          break;
-          default:
-            $speech="Sorry,I didnt get that .Please ask me something else";
-            break;
-  }
-  $response=new \stdClass();
+  
+  $response=new \stdClass();  
   $response->speech=$speech;
-  $response->displayText=$speech;
-  $response->source="webhook";
+  //$response->source="webhook";
   $response->data="google:{
         expect_user_response: true,
         is_ssml: true
